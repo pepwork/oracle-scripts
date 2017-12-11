770929bdfa87a4c3914c02891f9c391ec9a871b4
Perry Pakull <perry.pakull@trivadis.com>
2017-12-11 11:03:24 +0100

    Changed compiler

--- scripts/sqlplus/compiler.sql
+++ scripts/sqlplus/compiler.sql
@@ -1,72 +1,78 @@
-rem ------------------------------------------------------------------
-rem  Trivadis AG, Oracle Application Development
-rem ------------------------------------------------------------------
-rem  Projekt..........: PeP Development
-rem  Script...........: compiler.sql
-rem  Developer........: Perry Pakull (PeP), perry.pakull@trivadis.com
-rem  Date.............: 15.05.2013
-rem  Version..........: 2.0.0
-rem  Parameter........: -
-rem  Output...........: -
-rem  Description......: Compiling invalid objects of logon user.
-rem ------------------------------------------------------------------
-rem  Version Date       Who      What
-rem  2.0.0   15.05.2013 pep      Created
-rem ------------------------------------------------------------------
+----------------------------------------------------------------------
+--  GitHub https://github.com/pepwork
+----------------------------------------------------------------------
+--  Projekt..........: PeP Development
+--  Script...........: compiler.sql
+--  Developer........: Perry Pakull (PeP), perry.pakull@trivadis.com
+--  Date.............: 15.05.2013
+--  Version..........: 2.0.0
+--  Parameter........: -
+--  Output...........: -
+--  Description......: Compiling invalid objects of logon user.
+----------------------------------------------------------------------
+--  Version Date       Who      What
+--  2.0.0   15.05.2013 pep      Created
+----------------------------------------------------------------------
 
-set serveroutput on
-
-set heading off
+set serveroutput on size unlimited
 set feedback off
-
 declare
-   l_stmt                     varchar2 (32767);
+   l_count_invalid            pls_integer;
+   l_stmt                     varchar2(32767);
 begin
-   dbms_output.put_line ('Compiling invalid objects for user ' || user);
-   for r in (select object_name,
-                    object_type
-               from user_objects
-              where status = 'INVALID'
-             order by object_type,
-                      object_name)
-   loop
-      dbms_output.put_line('Compile ' || r.object_type || ' ' || r.object_name);
-      l_stmt := null;
-      if r.object_type = 'JAVA CLASS'
-      then
-         l_stmt := 'alter java class ' || user || '.' || r.object_name || ' compile';
-      elsif r.object_type = 'PACKAGE BODY'
-      then
-         l_stmt := 'alter package ' || r.object_name || ' compile body';
-      elsif r.object_type = 'INDEX'
-      then
-         l_stmt := 'alter index ' || r.object_name || ' rebuild';
-      elsif r.object_type = 'TYPE BODY'
-      then
-         l_stmt := 'alter type ' || r.object_name || ' compile body';
-      else
-         l_stmt := 'alter ' || r.object_type || ' ' || r.object_name || ' compile';
-      end if;
-      dbms_output.put_line(l_stmt);
-      begin
-         execute immediate l_stmt;
-      exception
-         when others
+   dbms_output.put_line ('Searching invalid objects for user ' || user);
+   select count(*)
+     into l_count_invalid
+     from user_objects
+    where status = 'INVALID';
+   dbms_output.put_line ('Found ' || to_char(l_count_invalid) ||' invalid objects for user ' || user);
+   if l_count_invalid > 0
+   then
+      dbms_output.put_line ('Compiling invalid objects for user ' || user);
+      for r in (select object_name,
+                       object_type
+                  from user_objects
+                 where status = 'INVALID'
+                 order by object_type,
+                          object_name)
+      loop
+         dbms_output.put_line('Compile ' || r.object_type || ' ' || r.object_name);
+         l_stmt := null;
+         if r.object_type = 'JAVA CLASS'
          then
-            null;
-      end;
-   end loop;
+            l_stmt := 'alter java class ' || user || '.' || r.object_name || ' compile';
+         elsif r.object_type = 'PACKAGE BODY'
+         then
+            l_stmt := 'alter package ' || r.object_name || ' compile body';
+         elsif r.object_type = 'INDEX'
+         then
+            l_stmt := 'alter index ' || r.object_name || ' rebuild';
+         elsif r.object_type = 'TYPE BODY'
+         then
+            l_stmt := 'alter type ' || r.object_name || ' compile body';
+         else
+            l_stmt := 'alter ' || r.object_type || ' ' || r.object_name || ' compile';
+         end if;
+         dbms_output.put_line(l_stmt);
+         begin
+            execute immediate l_stmt;
+         exception
+            when others
+            then
+               dbms_output.put_line('Error compiling object ' || r.object_name);
+               dbms_output.put_line(sqlerrm);
+         end;
+      end loop;
+      dbms_output.put_line ('Invalid objects for user ' || user || ' after compile.');
+      for r in (select object_name,
+                       object_type
+                  from user_objects
+                 where status = 'INVALID'
+                 order by object_type,
+                          object_name)
+      loop
+         dbms_output.put_line(r.object_type || ' ' || r.object_name);
+      end loop;
+   end if;
 end;
 /
-
-set heading on
-set feedback 6
-
-prompt
-prompt Invalid Objects after compile ...
-prompt
-col object_name format a50
-select object_type,
-       object_name
-  from user_objects
- where status = 'INVALID';
